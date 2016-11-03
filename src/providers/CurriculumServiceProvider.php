4400c9c9b9b8742f40e0172586494d9b74811f6f
Roger Melich <rogermelich@gmail.com>
2016-11-03 21:13:20 +0100

    Add Service Provider

---
+++ src/providers/CurriculumServiceProvider.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Scool\Curriculum\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class CurriculumServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+
+    }
+
+    public function boot()
+    {
+
+    }
+
+}
